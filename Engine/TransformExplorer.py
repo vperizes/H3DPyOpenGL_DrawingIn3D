dfcc4e8012fa54753577995b2afdad5a5e8d6c52
vperizes <vperizes@gmail.com>
2023-05-01 17:17:11 -0500

    Updated TransformExplorer.py to move camera with key presses

--- Engine/TransformExplorer.py
+++ Engine/TransformExplorer.py
@@ -16,6 +16,8 @@ pygame.display.set_caption('Transformations in Python')
 cube = Cube(GL_LINE_LOOP)
 mesh = LoadMesh("teapot.obj", GL_LINE_LOOP)
 
+eye = [0, 0, 0]  # initial camera positions that get updated
+
 def initialise():
     glClearColor(background_color[0], background_color[1], background_color[2], background_color[3])
     glColor(drawing_color)
@@ -25,6 +27,8 @@ def initialise():
     glLoadIdentity()
     gluPerspective(60, (screen_width / screen_height), 0.1, 500.0)
 
+
+def init_camera():
     # modelview
     glMatrixMode(GL_MODELVIEW)  # this switches open GL to model view mode. Any operations after this line are applied
     # to model view matrix
@@ -33,7 +37,7 @@ def initialise():
     # push model back to make it visible. Default: pos x is to the right and pos y is up
     glViewport(0, 0, screen.get_width(), screen.get_height())
     glEnable(GL_DEPTH_TEST)
-    gluLookAt(0, 10, 5, 0, 0, 0, 0, 1, 0)  # we change the camera transform in the model view. gluLookAt uses the
+    gluLookAt(eye[0], eye[1], eye[2], 0, 0, 0, 0, 1, 0)  # we change the camera transform in the model view. gluLookAt uses the
     # following format (eye.x, eye.y, eye.z, look.x, look.y, look.z, up.x, up.y, up.z). 'eye' refers to the eye/camera
     # location. 'look' refers to the point the camera is looking at (here we look at the origin where the teapot
     # is). 'up' refers to the orientation of camera
@@ -41,6 +45,7 @@ def initialise():
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    init_camera()
     glPushMatrix()
     mesh.draw()
     glPopMatrix()
@@ -52,7 +57,12 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_DOWN]:
+        eye[2] += 1  # this moves the camera back in the pos z dir
+    elif keys[pygame.K_UP]:
+        eye[2] -= 1  # this moves camera up in the pos y dir
     display()
     pygame.display.flip()
-    pygame.time.wait(100)
+    # pygame.time.wait(10)
 pygame.quit()
