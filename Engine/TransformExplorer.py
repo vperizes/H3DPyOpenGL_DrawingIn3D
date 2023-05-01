485b40bb02dd10fa6500adb3658b842beec31b84
vperizes <vperizes@gmail.com>
2023-05-01 12:56:57 -0500

    Starter code. Used to manipulate/understand projection matrix vs. model view

---
+++ Engine/TransformExplorer.py
@@ -0,0 +1,51 @@
+from pygame.locals import *
+from OpenGL.GLU import *
+from Cube import *
+from LoadMesh import *
+
+pygame.init()
+
+# project settings
+screen_width = 1000
+screen_height = 800
+background_color = (0, 0, 0, 1)
+drawing_color = (1, 1, 1, 1)
+
+screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption('Transformations in Python')
+cube = Cube(GL_LINE_LOOP)
+mesh = LoadMesh("cube.obj", GL_LINE_LOOP)
+
+def initialise():
+    glClearColor(background_color[0], background_color[1], background_color[2], background_color[3])
+    glColor(drawing_color)
+
+    # projection
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(60, (screen_width / screen_height), 0.1, 500.0)
+
+    # modelview
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glViewport(0, 0, screen.get_width(), screen.get_height())
+    glEnable(GL_DEPTH_TEST)
+
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glPushMatrix()
+    mesh.draw()
+    glPopMatrix()
+
+
+done = False
+initialise()
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    display()
+    pygame.display.flip()
+    pygame.time.wait(100)
+pygame.quit()
